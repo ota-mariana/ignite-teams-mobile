7c7698fc30527e080b60763c2f3950f08791bce5
Mariana <nanomarii10@gmail.com>
2023-04-26 15:28:28 -0300

    feat: Loading component and font style

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { Groups } from '@screens/Groups';
-import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
+import theme from './src/theme';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      {
+        fontsLoaded ? <Groups /> : <Loading />
+      }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${props => props.theme.COLORS.GRAY_600};
   align-items: center;
   justify-content: center;
 `;
