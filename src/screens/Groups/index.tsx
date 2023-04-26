c8e9bd257da69a3eb6e7a96d041c97a42b6b4f54
Mariana <nanomarii10@gmail.com>
2023-04-26 17:37:03 -0300

    feat: HighLight component creation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { HighLight } from '@components/HighLight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
+      <HighLight
+        title="Times"
+        subtitle="Jogue com o seu time!"
+      />
     </Container>
   );
 }
