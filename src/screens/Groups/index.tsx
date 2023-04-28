65cd854c2804715d60e50cf75169c15b50776048
Mariana <nanomarii10@gmail.com>
2023-04-28 13:28:18 -0300

    feat: starting the highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { HighLight } from '@components/HighLight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -11,6 +12,8 @@ export function Groups() {
         title="Times"
         subtitle="Jogue com o seu time!"
       />
+
+      <GroupCard title="Xablau na face"/>
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: ${props => props.theme.COLORS.GRAY_600};
+  padding: 28px;
   /* align-items: center;
   justify-content: center; */
 `;
