4e856bb7eabd2efe04b2d194a8ca3bba3ccdcaab
Mariana Ota <marianaota@Marianas-MacBook-Air.local>
2023-05-25 21:42:45 -0300

    feat: PlayerCard component creation

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
-import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 import { HighLight } from '@components/HighLight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
-import { FlatList } from 'react-native';
-import { useState } from 'react';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
+import { Button } from '@components/Button';
 
 export function Players() {
 	const [team, setTeam] = useState('Time A');
-	const [players, setPlayers] = useState(['Mari']);
+	const [players, setPlayers] = useState([]);
 	return (
 		<Container>
 			<Header showBackButton />
@@ -42,6 +47,28 @@ export function Players() {
 					{ players.length }
 				</NumbersOfPlayers>
 			</HeaderList>
+
+			<FlatList
+				data={players}
+				keyExtractor={item => item}
+				renderItem={({ item }) => (
+					<PlayerCard
+						name={item}
+						onRemove={() => { }}
+					/>
+				)}
+				ListEmptyComponent={() => <ListEmpty message="Não há pessoas nesse time." />}
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={[
+					{paddingBottom: 100},
+					players.length === 0 && { flex: 1 }
+				]}
+			/>
+
+			<Button
+			title='Remover Turma'
+				type='SECONDARY'
+			/>
 		</Container>
 	)
 }
